5827e7004151384bc9773820d033ae29967e6e29
erik javaloya <bis_erik@hotmail.com>
2017-06-24 17:21:48 +0200

    Mi primer archivo incorporado al repositiorio

---
+++ ServletManual.java
@@ -0,0 +1,24 @@
+package juandegaray.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+public class ServletManual extends HttpServlet{
+ 
+ @Override
+ protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+                  throws ServletException, IOException {
+           
+            PrintWriter out = resp.getWriter();
+            out.println("<html>");
+            out.println("<body>");
+            out.println("hoy es " + new Date());
+            out.println("</body>");
+            out.println("</html>");
+	}
+}
